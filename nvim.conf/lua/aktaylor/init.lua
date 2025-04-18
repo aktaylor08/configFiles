0504b3a540c63ea1c742d70281a5535941ff691c
Adam Taylor <aktaylor08@gmail.com>
2025-04-18 15:42:51 -0500

    init working nvim config

---
+++ nvim.conf/lua/aktaylor/init.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " " -- Set leader key before Lazy
+ 
+require("aktaylor.lazy_init")
+
+vim.cmd[[colorscheme tokyonight]]
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
